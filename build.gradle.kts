87364c880506af4fde8aaf7690f060b4b55fb27d
zeromake <a390720046@gmail.com>
2022-07-19 18:01:54 +0800

    fix: support android studio stable

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
 
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0-alpha08")
+        classpath("com.android.tools.build:gradle:7.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("io.objectbox:objectbox-gradle-plugin:3.1.2")
     }
